2684ac90741bae5ba33dd0c7a13b73810135f630
Markus Ongyerth <ongy@ongy.net>
2017-11-08 12:53:41 +0100

    Make the placing logic smarter for multiple outputs on same workspace

--- src/Layout.hs
+++ src/Layout.hs
@@ -1,5 +1,7 @@
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TupleSections #-}
 module Layout
     ( reLayout
     )
@@ -10,17 +12,38 @@ import Control.Monad.IO.Class (liftIO)
 import Data.IORef (modifyIORef, readIORef)
 import System.IO (hPutStrLn, stderr)
 
+import Graphics.Wayland.WlRoots.Box (WlrBox (..), centerBox)
 import Graphics.Wayland.WlRoots.Output (getOutputBox, getOutputName)
 
 import Utility (whenJust, intToPtr)
 import View (setViewBox)
 import ViewSet (WSTag (..), Workspace (..), Layout (..), pureLayout)
 import Waymonad (Way, WayBindingState (..), getState)
---import WayUtil (getViewSet)
 
 import qualified Data.IntMap.Strict as IM
 import qualified Data.Map.Strict as M
 import qualified Data.Text.IO as T
+
+getBoxes
+    :: WSTag a
+    => a
+    -> Way a [(Int, WlrBox)]
+getBoxes ws = do
+    xs <- liftIO . readIORef . wayBindingMapping =<< getState
+    let outputs = map snd . filter ((==) ws . fst) $ xs
+    liftIO $ mapM (\out -> fmap (out,) . getOutputBox $ intToPtr out) outputs
+
+getLayoutBoxes
+    :: WSTag a
+    => a
+    -> Way a [(Int, WlrBox)]
+getLayoutBoxes ws = do
+    outs <- getBoxes ws
+
+    let smallest :: WlrBox = foldr shrink (WlrBox 0 0 maxBound maxBound) $ map snd outs
+    pure $ map (fmap $ centerBox smallest) outs
+    where   shrink :: WlrBox -> WlrBox -> WlrBox
+            shrink (WlrBox _ _ lw lh) (WlrBox _ _ rw rh) = WlrBox 0 0 (min lw rw) (min lh rh)
 
 reLayout
     :: WSTag a
@@ -29,13 +52,13 @@ reLayout
 reLayout ws = do
     state <- getState
     wstate <- M.lookup ws <$> (liftIO . readIORef . wayBindingState $ state)
-    xs <- liftIO . readIORef $ wayBindingMapping state
     let cacheRef = wayBindingCache state
 
-    liftIO $ forM_ (map snd $ filter ((==) ws . fst) xs) $ \out -> whenJust wstate $ \case
+    boxes <- getLayoutBoxes ws
+
+    liftIO $ forM_ boxes $ \(out, box) -> whenJust wstate $ \case
         (Workspace _ Nothing) -> modifyIORef cacheRef $ IM.delete out
         (Workspace (Layout l) (Just vs)) -> do
-            box <- getOutputBox $ intToPtr out
             let layout = pureLayout l box vs
             modifyIORef cacheRef $ IM.insert out layout
 
