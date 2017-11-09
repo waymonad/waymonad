f8dae4e904b4cb4a1af7fc0521ffd9e6629b3ab5
Markus Ongyerth <ongy@ongy.net>
2017-11-09 23:13:14 +0100

    Add license_helper.sh
    License_helper is a simple script that makes sure every file contains a
    license header.
    It takes the current year and and uses the name configured as the
    current git user.
    
    In the long run I would like to improve on this by getting contributers
    and years a file changed in from git and making sure the license headers
    are always up to date in the script.

---
+++ license_helper.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+set -e -u
+
+if [ -n "`git status --porcelain`" ]
+then
+	echo "This script will modify your state, therefore I will refuse \
+to run on a dirty git repository"
+	exit 1
+fi
+
+# If I need to generalise this
+REACH="Reach us at https:\\/\\/github.com\\/ongy\\/waymonad"
+PROJECT="waymonad A wayland compositor in the spirit of xmonad"
+YEAR=`date "+%Y"`
+NAME=`git config user.name`
+
+FILES=`find src -type f`
+
+for file in ${FILES}
+do
+	TFILE="${file}.license_tmp"
+	# For now we skip everything that contains a license header already.
+	# Maybe I'll change this later (probably not in bash)
+	if grep "Copyright (C)" "${file}"
+	then
+		continue
+	fi
+
+	cp "${file}" "${TFILE}"
+
+	sed \
+		-e "s/{{YEAR}}/${YEAR}/" \
+		-e "s/{{NAME}}/${NAME}/" \
+		-e "s/{{PROJECT}}/${PROJECT}/" \
+		-e "s/{{REACH}}/${REACH}/" \
+		<"./.license_template" \
+		>"${file}"
+
+	cat "${TFILE}" >> "${file}"
+	unlink "${TFILE}"
+done
