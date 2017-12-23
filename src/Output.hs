5d55c9ca2b18e2faeac592d8150a4c36cec915d3
Markus Ongyerth <ongy@ongy.net>
2017-12-23 11:46:40 +0100

    Fix positioning of subsurfaces with forced scaling

--- src/Output.hs
+++ src/Output.hs
@@ -160,8 +160,8 @@ outputHandleSurface comp secs output surface scaleFactor box = do
                     output
                     subsurf
                     scaleFactor
-                    sbox{ boxX = boxX sbox + boxX box
-                        , boxY = boxY sbox + boxY box
+                    sbox{ boxX = floor (fromIntegral (boxX sbox) * scaleFactor) + boxX box
+                        , boxY = floor (fromIntegral (boxY sbox) * scaleFactor) + boxY box
                         , boxWidth = floor $ fromIntegral (boxWidth sbox) * scaleFactor
                         , boxHeight = floor $ fromIntegral (boxHeight sbox) * scaleFactor
                         }
