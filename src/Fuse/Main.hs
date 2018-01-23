90d9765d4aacfc8f9b262efac8d23b0b49ffe52f
Markus Ongyerth <ongy@ongy.net>
2018-01-24 00:33:35 +0100

    Disable Fuse RTS-Foo, for older compilier

--- src/Fuse/Main.hs
+++ src/Fuse/Main.hs
@@ -48,7 +48,7 @@ import Fuse.Inputs
 import Fuse.Outputs
 import Fuse.Workspaces
 import Fuse.Shells
-import Fuse.Rts
+-- import Fuse.Rts
 
 
 import qualified Data.Map as M
@@ -103,7 +103,7 @@ mainDir = simpleDir $ M.fromList
     , ("shutdown", closeFile)
     , ("inputs", inputsDir)
     , ("shells", shellsDir)
-    , ("rts", rtsDir)
+    -- , ("rts", rtsDir)
     ]
 
 
